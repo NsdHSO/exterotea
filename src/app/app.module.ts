c51b48bc49f7d3ead38473a4c010c62f2c74361e
(Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2022-07-19 02:42:57 +0300

    feat(frame): added frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableMaterialModule } from 'ngx-liburg';
 import { StoreModule } from '@ngrx/store';
+import {IconCoreModule} from "ngx-liburg-icon";
+import {FrameWholeModule, ParentRouter} from "ngx-liburg-frame-side";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,45 @@ import { StoreModule } from '@ngrx/store';
     BrowserModule,
     AppRoutingModule,
     TableMaterialModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    IconCoreModule,
+    FrameWholeModule.forRoot({
+      routerDataConfig: [{
+        path: "asdasd",
+        icon: "fa_solid:arrow-down",
+        text: "dasdasd",
+        subRouter: [{
+          path: "test",
+          icon: "fa_solid:person-through-window",
+          text: "dasdasd",
+        } as ParentRouter]
+      },
+        {
+          path: "reasdasd",
+          icon: "fa_solid:person-through-window",
+          text: "dasdasd",
+
+          subRouter: [{
+            path: "asdad",
+            icon: "fa_solid:person-through-window",
+            text: "dasdasd",
+          } as ParentRouter,
+            {
+              path: "asda",
+              icon: "fa_solid:person-through-window",
+              text: "dasdasd",
+            } as ParentRouter,{
+              path: "dda",
+              icon: "fa_solid:person-through-window",
+              text: "dasdasd",
+            } as ParentRouter,{
+              path: "ddd",
+              icon: "fa_solid:person-through-window",
+              text: "dasdasd",
+            } as ParentRouter]
+        }],
+      iconApp: 'fa_solid:address-book'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]

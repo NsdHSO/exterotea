ed70e7507d661dfb1a69a8d2f6989f3263156616
(Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2022-07-19 12:18:13 +0300

    chore(header): added header

---
+++ src/app/@core/routerConfig.ts
@@ -0,0 +1,11 @@
+import {ParentRouter} from "ngx-liburg-frame-side";
+
+export const CONFIG_MAIN = Object.freeze({
+  routerDataConfig: [{
+    path: 'asdasd',
+    icon: 'fa_solid:arrow-down',
+    text: 'dasdasd',
+  },
+  ],
+  iconApp: ''
+})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TableMaterialModule } from 'ngx-liburg';
-import { StoreModule } from '@ngrx/store';
-import { IconCoreModule } from 'ngx-liburg-icon';
-import { FrameWholeModule, ParentRouter } from 'ngx-liburg-frame-side';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TableMaterialModule} from 'ngx-liburg';
+import {StoreModule} from '@ngrx/store';
+import {IconCoreModule} from 'ngx-liburg-icon';
+import {FrameWholeModule, ParentRouter} from 'ngx-liburg-frame-side';
+import {CustomHeaderComponent} from './custom-header/custom-header.component';
+import {CONFIG_MAIN} from "./@core/routerConfig";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomHeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -18,43 +21,8 @@ import { FrameWholeModule, ParentRouter } from 'ngx-liburg-frame-side';
     TableMaterialModule,
     StoreModule.forRoot({}, {}),
     IconCoreModule,
-    FrameWholeModule.forRoot({
-      routerDataConfig: [{
-        path: 'asdasd',
-        icon: 'fa_solid:arrow-down',
-        text: 'dasdasd',
-        subRouter: [{
-          path: 'test',
-          icon: 'fa_solid:person-through-window',
-          text: 'dasdasd'
-        } as ParentRouter]
-      },
-      {
-        path: 'reasdasd',
-        icon: 'fa_solid:person-through-window',
-        text: 'dasdasd',
-
-        subRouter: [{
-          path: 'asdad',
-          icon: 'fa_solid:person-through-window',
-          text: 'dasdasd'
-        } as ParentRouter,
-            {
-              path: 'asda',
-              icon: 'fa_solid:person-through-window',
-              text: 'dasdasd'
-            } as ParentRouter, {
-              path: 'dda',
-              icon: 'fa_solid:person-through-window',
-              text: 'dasdasd'
-            } as ParentRouter, {
-              path: 'ddd',
-              icon: 'fa_solid:person-through-window',
-              text: 'dasdasd'
-            } as ParentRouter]
-      }],
-      iconApp: ''
-    })
+    FrameWholeModule.forRoot(CONFIG_MAIN
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]

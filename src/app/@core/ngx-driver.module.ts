47063f01c9d08a25574450b0dae63eff4bae8c7a
(Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2022-09-15 15:00:36 +0300

    feat: added env variable

--- src/app/@core/ngx-driver.module.ts
+++ src/app/@core/ngx-driver.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CoreDriverModule } from 'ngx-driver';
-import { environment } from "../../environments/environment";
 
 @NgModule({
   declarations: [],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from "ngx-driver";
+import { environment } from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,8 @@ import { Component } from '@angular/core';
  */
 export class AppComponent {
   title = 'exterotea';
+
+  constructor(private readonly _localStorage: LocalStorageService) {
+    this._localStorage.setItem('token', environment.token)
+  }
 }

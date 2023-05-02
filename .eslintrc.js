06cbe9cc201ac375befe52c162ff176493f1a2cd
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-05-02 18:39:06 +0300

    chore:🌻 added tooltip component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,43 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true
   },
-  'extends': [
-    'google'
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  extends: [ 'google' ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  'plugins': [
-    '@typescript-eslint'
-  ],
-  'rules': {
+  plugins: [ '@typescript-eslint' ],
+  rules: {
     'no-console': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'warn',
     '@typescript-eslint/no-useless-empty-export': 'warn',
-    'comma-dangle': ['error', 'never'],
-    'indent': [
-      'error',
-      2
-    ],
-    'max-len': [
-      'error', {
-        'code': 200
+    'comma-dangle': [ 'error', 'never' ],
+    'array-bracket-spacing': [ 1, 'always' ],
+    'indent': [ 'error', 2 ],
+    'newline-per-chained-call': [ 'error', { 'ignoreChainWithDepth': 2 } ],
+    'require-jsdoc': [
+      'warn',
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: true,
+          FunctionExpression: true
+        }
       }
     ],
-    'object-curly-spacing': [2, 'always'],
+    'max-len': [
+      'error',
+      {
+        code: 200
+      }
+    ],
+    'object-curly-spacing': [ 2, 'always' ],
     'new-cap': 0
   }
 };
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
-import {NgModule} from '@angular/core';
-import {
-  RouterModule,
-  Routes
-} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes : Routes = [
+const routes: Routes = [
   {
     path: 'product',
     loadChildren: () => import('./@core/product.module').then(
@@ -59,9 +56,10 @@ const routes : Routes = [
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports: [RouterModule]
+  exports: [ RouterModule ]
 })
 /** *************************************************************************************************
  * App module routing
  */
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}

7962cdf345ffb37c8d3a9cb81a0d345374ed258c
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-05-04 05:11:41 +0300

    chore:🌻 added some stuf for get middle

--- src/app/shared/tooltip/get-middle-entity.directive.ts
+++ src/app/shared/tooltip/get-middle-entity.directive.ts
@@ -22,12 +22,18 @@ import { debounceTime, fromEvent, Subject, takeUntil } from 'rxjs';
 export class GetMiddleEntityDirective {
   @Input() element: any;
   @ViewChild('rendererTemplate') rendererTemplate!: TemplateRef<any>;
-  public middlex: number =0;
-
+  public middlex: number = 0;
+  bottomOffset = 10; // define bottomOffset property
+  topOffset = 10; // define topOffset property
   private componentRef: ComponentRef<TooltipComponent> | any;
   private tooltipRef: HTMLElement | any;
   private clickEventListener: (() => void) | null = null;
   private _destroyes$ = new Subject();
+
+  get hostElement(): HTMLElement{
+    return this.elementRef.nativeElement;
+  }
+
   constructor(
     private appRef: ApplicationRef,
     private elementRef: ElementRef,
@@ -35,20 +41,16 @@ export class GetMiddleEntityDirective {
     private injector: Injector,
     private renderer: Renderer2,
     private viewContainerRef: ViewContainerRef
-  ) {
-  }
-  get hostElement(): HTMLElement {
-    return this.elementRef.nativeElement;
+  ){
   }
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit(): void{
     this.middlex = this.elementRef.nativeElement.offsetWidth / 2 + this.elementRef.nativeElement.offsetLeft;
   }
 
-
   @HostListener('click', [ '$event' ])
-  onClick(event: MouseEvent) {
-    if (!this.componentRef) {
+  onClick(event: MouseEvent){
+    if ( !this.componentRef ) {
       const hostRect = this.elementRef.nativeElement.getBoundingClientRect();
       const middlex = hostRect.left + hostRect.width / 2;
       this.renderComponent(middlex, 'Tooltip Text');
@@ -56,10 +58,11 @@ export class GetMiddleEntityDirective {
         window,
         'click',
         (clickEvent: MouseEvent) => {
-          const domElem = (this.componentRef.hostView as any).rootNodes[0] as HTMLElement;
+          const domElem = (this.componentRef.hostView as any).rootNodes[ 0 ] as HTMLElement;
           domElem.style.left = `${ middlex - domElem.getBoundingClientRect().width / 2 }px`;
 
-          if (!this.elementRef.nativeElement.contains(clickEvent.target) && !this.tooltipRef?.contains(clickEvent.target as HTMLElement)) {
+          if ( !this.elementRef.nativeElement.contains(clickEvent.target) && !this.tooltipRef?.contains(
+            clickEvent.target as HTMLElement) ) {
             this.destroyComponent();
           }
         });
@@ -72,50 +75,57 @@ export class GetMiddleEntityDirective {
           const middlex = hostRect.left + hostRect.width / 2;
           const tooltipRect = this.tooltipRef.getBoundingClientRect();
 
-          if (window.innerHeight < tooltipRect.bottom) {
+          if ( window.innerHeight < tooltipRect.bottom ) {
             // no space below, reposition to the top
-            this.tooltipRef.style.top = `${hostRect.top - tooltipRect.height}px`;
+            this.tooltipRef.style.top = `${ hostRect.top - tooltipRect.height }px`;
           } else {
             // position below the host element
-            this.tooltipRef.style.top = `${hostRect.bottom}px`;
+            this.tooltipRef.style.top = `${ hostRect.bottom }px`;
           }
 
-          if (window.innerWidth < tooltipRect.right) {
+          if ( window.innerWidth < tooltipRect.right ) {
             // no space on the right, reposition to the left
-            this.tooltipRef.style.left = `${hostRect.right - tooltipRect.width}px`;
-          } else if (middlex > tooltipRect.width / 2 && window.innerWidth - middlex > tooltipRect.width / 2) {
+            this.tooltipRef.style.left = `${ hostRect.right - tooltipRect.width }px`;
+          } else if ( middlex > tooltipRect.width / 2 && window.innerWidth - middlex > tooltipRect.width / 2 ) {
             // position centered over the host element
-            this.tooltipRef.style.left = `${middlex - tooltipRect.width / 2}px`;
+            this.tooltipRef.style.left = `${ middlex - tooltipRect.width / 2 }px`;
           } else {
             // position on the right of the host element
-            this.tooltipRef.style.left = `${hostRect.left}px`;
+            this.tooltipRef.style.left = `${ hostRect.left }px`;
           }
         });
-    } else if (!this.tooltipRef?.contains(event.target as HTMLElement)) {
+    } else if ( !this.tooltipRef?.contains(event.target as HTMLElement) ) {
       this.destroyComponent();
     }
   }
-  private renderComponent(middlex: number, text: string) {
+
+  ngDestroy(){
+    this._destroyes$.next(true);
+    this._destroyes$.complete();
+  }
+
+  private renderComponent(middlex: number, text: string){
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
       TooltipComponent);
     this.componentRef = componentFactory.create(this.injector);
     this.componentRef.instance.text = text;
     this.componentRef.instance.rendererTemplate = this.element;
     this.appRef.attachView(this.componentRef.hostView);
-    const domElem = (this.componentRef.hostView as any).rootNodes[0] as HTMLElement;
+    const domElem = (this.componentRef.hostView as any).rootNodes[ 0 ] as HTMLElement;
     document.body.appendChild(domElem);
     const hostRect = this.elementRef.nativeElement.getBoundingClientRect();
     domElem.style.position = 'fixed';
-    domElem.style.top = `${hostRect.bottom}px`;
+    domElem.style.top = `${ hostRect.bottom }px`;
     domElem.style.zIndex = '100';
     this.tooltipRef = domElem;
 
     // Subscribe to window resize events
     const resizeSubscription = fromEvent(window, 'resize').pipe(
       debounceTime(50)
-    ).subscribe(() => {
-      this.updatePosition();
-    });
+    )
+      .subscribe(() => {
+        this.updatePosition();
+      });
 
     // Unsubscribe from the subscription when the tooltip is destroyed
     this.componentRef.onDestroy(() => {
@@ -123,29 +133,29 @@ export class GetMiddleEntityDirective {
     });
   }
 
-  private updatePosition() {
-    if (this.tooltipRef) {
+  private updatePosition(){
+    if ( this.tooltipRef ) {
       const hostRect = this.elementRef.nativeElement.getBoundingClientRect();
       const tooltipHeight = this.tooltipRef.getBoundingClientRect().height;
       const windowHeight = window.innerHeight;
       const availableBottomSpace = windowHeight - hostRect.bottom - this.bottomOffset;
       const availableTopSpace = hostRect.top - this.topOffset;
 
-      if (availableBottomSpace < tooltipHeight && availableTopSpace >= tooltipHeight) {
-        this.tooltipRef.style.top = `${hostRect.top - tooltipHeight - this.topOffset}px`;
+      if ( availableBottomSpace < tooltipHeight && availableTopSpace >= tooltipHeight ) {
+        this.tooltipRef.style.top = `${ hostRect.top - tooltipHeight - this.topOffset }px`;
         this.tooltipRef.classList.add('top');
         this.tooltipRef.classList.remove('bottom');
       } else {
-        this.tooltipRef.style.top = `${hostRect.bottom + this.bottomOffset}px`;
+        this.tooltipRef.style.top = `${ hostRect.bottom + this.bottomOffset }px`;
         this.tooltipRef.classList.add('bottom');
         this.tooltipRef.classList.remove('top');
       }
 
-      this.tooltipRef.style.left = `${hostRect.left + hostRect.width / 2}px`;
+      this.tooltipRef.style.left = `${ hostRect.left + hostRect.width / 2 }px`;
     }
   }
 
-  private destroyComponent() {
+  private destroyComponent(){
     if ( this.componentRef ) {
       this.appRef.detachView(this.componentRef.hostView);
       this.componentRef.destroy();
@@ -155,10 +165,5 @@ export class GetMiddleEntityDirective {
         this.clickEventListener = null;
       }
     }
-  }
-
-  ngDestroy() {
-    this._destroyes$.next(true);
-    this._destroyes$.complete();
   }
 }
